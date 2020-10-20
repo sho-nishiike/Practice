5834c612d996b87d0d745b095a504bec1b98475f
VGS-TSUKISHIMA\sho-nishiike <sho-nishiike@soleilgamestudios.com>
2020-10-20 17:05:00 +0900

    Added my code.

---
+++ Assets/Scripts/Runtime/testSN.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSN : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("Start test");
+    }
+
+	private float _updateTime = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+		_updateTime += Time.deltaTime;
+		if (_updateTime >= 1.0f)
+		{
+			_updateTime -= 1.0f;
+			Debug.Log("Update");
+		}
+    }
+}
