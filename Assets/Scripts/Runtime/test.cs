226383e22c738f827c8e7b9715ca64df90771676
VGS-TSUKISHIMA\i-okumura <i-okumura@soleilgamestudios.com>
2020-10-21 14:47:39 +0900

    changed hoge to _hoge

--- Assets/Scripts/Runtime/test.cs
+++ Assets/Scripts/Runtime/test.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    private string hoge = "Hello, World!!!";
+    private string _hoge = "Hello, World!!!";
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(hoge);
+        Debug.Log(_hoge);
     }
     
 }
